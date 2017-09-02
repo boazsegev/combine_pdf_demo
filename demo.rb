c4bee000ce27637dd4759f43ac604bb1f8c74a35
Bo <Boaz@2be.co.il>
2017-09-01 21:45:51 -0400

    Bump versions

--- demo.rb
+++ demo.rb
@@ -5,17 +5,18 @@
 require 'pathname'
 ## Set up root object, it might be used by the environment and\or the plezi extension gems.
 Root ||= Pathname.new(File.dirname(__FILE__)).expand_path
+
+## If this app is independant, use bundler to load gems (including the plezi gem).
+## Else, use the original app's Gemfile and start Plezi's Rack mode.
+require 'bundler'
+Bundler.require(:default, ENV['ENV'].to_s.to_sym)
+
 ## make sure all file access and file loading is relative to the application's root folder
 # Dir.chdir Root.to_s
 ## load code from a subfolder called 'code'
 # Dir[File.join "{code}", "**" , "*.rb"].each {|file| load File.expand_path(file)}
 # load Root.join('pdf_controller.rb').to_s
 Dir[File.join File.dirname(__FILE__), '*.rb'].each { |file| load File.expand_path(file) unless file == __FILE__ }
-
-## If this app is independant, use bundler to load gems (including the plezi gem).
-## Else, use the original app's Gemfile and start Plezi's Rack mode.
-require 'bundler'
-Bundler.require(:default, ENV['ENV'].to_s.to_sym)
 
 # start a web service to listen on the first default port (3000 or the port set by the command-line).
 # you can change some of the default settings here.
